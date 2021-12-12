00a2121e789201a5a44dbb8c5497e1ed1b31fe3a
yuwenjie <v_yuwenjie@163.com>
2021-12-12 13:54:27 +0800

    idea整合git  test分支 由版本 v6 => v7

--- src/main/java/com/yu/IdeaIntegrationGit.java
+++ src/main/java/com/yu/IdeaIntegrationGit.java
@@ -8,5 +8,6 @@ public class IdeaIntegrationGit {
         System.out.println("test branch v4");
         System.out.println("test branch v5");
         System.out.println("test branch v6");
+        System.out.println("test branch v7");
     }
 }
