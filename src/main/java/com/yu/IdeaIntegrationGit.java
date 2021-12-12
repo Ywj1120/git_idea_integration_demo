7170315198279c0c6e2c704785d07d0da969a843
yuwenjie <v_yuwenjie@163.com>
2021-12-12 13:18:31 +0800

    idea整合git 版本由 v2 => v3

--- src/main/java/com/yu/IdeaIntegrationGit.java
+++ src/main/java/com/yu/IdeaIntegrationGit.java
@@ -4,5 +4,6 @@ public class IdeaIntegrationGit {
     public static void main(String[]args){
         System.out.println("123");
         System.out.println("v2");
+        System.out.println("v3");
     }
 }
